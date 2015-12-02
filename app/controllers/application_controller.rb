a47be4d8d3775db32ef1ec98599c87ade0a3250a
alex <alex@experiencefellow.com>
2015-12-02 11:11:23 +0100

    set current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
+
+	prepend_before_filter :set_current_user
+
+	private
+
+	def set_current_user
+		@current_user = User.find_by(:access_token => params[:access_token])
+		render :text => "access denied", :status => 403 if(current_user.nil?)
+	end
+
+	def current_user
+		@current_user
+	end
 end
